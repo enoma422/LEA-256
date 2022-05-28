45acf6291665858531e1891c46d11cd6bac5eeda
enoma422 <74201624+enoma422@users.noreply.github.com>
2022-05-29 02:22:29 +0900

    Update lea.h

--- WhiteBox_LEA/LEA_C/lea.h
+++ WhiteBox_LEA/LEA_C/lea.h
@@ -1,6 +1,5 @@
+// LEA_C.h
 #pragma once
-
-#include <string.h>
 #include <stdint.h>
 
 static const uint32_t Key_delta[8];
@@ -10,8 +9,9 @@ static uint32_t PLUS(uint32_t x, uint32_t y);
 static uint32_t MINUS(uint32_t x, uint32_t y);
 
 void LEA_Round_Enc(uint32_t *in_state, uint32_t *out_state, uint32_t *RK_enc);
+void LEA_Round_Dec(uint32_t *in_state, uint32_t *out_state, uint32_t *RK_enc);
 
-void KeySchedule_256_Enc(unsigned char *K, uint32_t *RoundKey);
+void KeySchedule_256(unsigned char *K, uint32_t *RoundKey);
 
 void encrypt_lea(int Nr, uint32_t *RK, unsigned char *PT, unsigned char *CT);
 void decrypt_lea(int Nr, uint32_t *RK, unsigned char *PT, unsigned char *CT);
